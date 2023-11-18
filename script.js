4bc6f410f0c5a832baf155165f7c1ff577f1a01b
Bogdan_Ishcenko <bogdan.ishchenko.852@gmail.com>
2023-11-18 18:20:11 +0300

    repair final

--- script.js
+++ script.js
@@ -9,106 +9,29 @@ readMoreButton.onclick = () =>{
     readMoreText.textContent = buttonIcon.classList.contains('read-more__icon--active') ? 'Скрыть':'Показать все'
 }
 
-
-
-    const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper', {
         
-        direction: 'horizontal',
-        loop: true,
-        freeMode:true,
-        centeredSliders:true,
-        autoHeight:true,
-        updateOnWindowResize: true,
-     
-        pagination: {
-            el: '.swiper-pagination',
-            clickable:true,
+    direction: 'horizontal',
+    loop: true,
+    freeMode:true,
+    centeredSliders:true,
+    autoHeight:true,
+    updateOnWindowResize: true,
+    spaceBetween: 16,
+    slidesPerView: "auto",
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+    },
+
+    breakpoints: {
+        768: {      
+            spaceBetween: 0,
+            enabled: false,
         },
-
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-                enabled: true,
-            },
-            320: {
-                slidesPerView: 1.25,
-                enabled: true,
-            },
-            345: {
-                slidesPerView: 1.35,
-                enabled: true,
-            },
-            370: {
-                slidesPerView: 1.45,
-                enabled: true,
-            },
-            395: {
-                slidesPerView: 1.55,
-                enabled: true,
-            },
-            420: {
-                slidesPerView: 1.65,
-                enabled: true,
-            },
-            445: {
-                slidesPerView: 1.75,
-                enabled: true,
-            },
-            470: {
-                slidesPerView: 1.85,
-                enabled: true,
-            },
-            495: {
-                slidesPerView: 1.95,
-                enabled: true,
-            },
-            520: {
-                slidesPerView: 2.05,
-                enabled: true,
-            },
-            545: {
-                slidesPerView: 2.15,
-                enabled: true,
-            },
-            570: {
-                slidesPerView: 2.25,
-                enabled: true,
-            },
-            595: {
-                slidesPerView: 2.35,
-                enabled: true,
-            },
-            620: {
-                slidesPerView: 2.45,
-                enabled: true,
-            },
-            645: {
-                slidesPerView: 2.55,
-                enabled: true,
-            },
-            670: {
-                slidesPerView: 2.65,
-                enabled: true,
-            },
-            695: {
-                slidesPerView: 2.75,
-                enabled: true,
-            },
-            720: {
-                slidesPerView: 2.85,
-                enabled: true,
-            },
-            745: {
-                slidesPerView: 2.95,
-                enabled: true,
-            },
-            767: {
-                
-                slidesPerView: "auto",
-                enabled: false,
-            },
             
-        }
+    }
         
-    })
+})
 
